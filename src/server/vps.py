9ee7075b88fbd81089aee99a6adbac7081ee874f
underground <underground@underground.pm>
2024-02-02 20:27:23 +0100

    update

--- src/server/vps.py
+++ src/server/vps.py
@@ -12,13 +12,42 @@ async def vps_server_create(name: str, server: Server) -> None | Exception:
             if avaible_cores =< server.cores:
                 raise Exception("Doesn't have avaible cores")
 
-            async with open("vps.xml") as file:
-                xml = file.read()
+            xml = f"""
+            <domain type='kvm'>
+                <name>{name}</name>
+                <memory unit='GiB'>{server.ram}</memory>
+                <vcpu placement='static'>{server.cores}</vcpu>
+                <devices>
+                    <emulator>/usr/bin/qemu-system-x86_64</emulator>
+                    <disk type='file' device='cdrom'>
+                        <driver name='qemu' type='raw'/>
+                        <source file='/var/lib/libvirt/iso/alpine-standard-3.19.0-x86_64.iso'/>
+                        <target dev='sdb' bus='sata'/>
+                        <readonly/>
+                        <boot order='1'/>
+                        <address type='drive' controller='0' bus='0' target='0' unit='1'/>
+                    </disk>
+                    <disk type='file' device='disk'>
+                        <driver name='qemu' type='qcow2' discard='unmap'/>
+                        <source file='/var/lib/libvirt/images/{name}.qcow2'/>
+                        <target dev='sda' bus='sata'/>
+                        <boot order='2'/>
+                        <address type='drive' controller='0' bus='0' target='0' unit='0'/>
+                    </disk>
+                    <interface type='network'>
+                        <mac address='52:54:00:4e:31:f9'/>
+                        <source network='default'/>
+                        <model type='e1000e'/>
+                        <address type='pci' domain='0x0000' bus='0x01' slot='0x00' function='0x0'/>
+                    </interface>
+                    <graphics type='vnc' port='-1' autoport='yes'>
+                        <listen type='address'/>
+                    </graphics>
+                </devices>
+            </domain>
+            """
 
             vps = conn.defineXML(xml)
-            vps.name(name)
-            vps.setVcpus(server.cores)
-            vps.setMemory(server.ram)
     except Exception as e:
         raise e
 

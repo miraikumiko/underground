a19ab1e16980032a1f3e6d5b9e2dc39a8cda5b5e
underground <underground@underground.pm>
2024-12-11 22:50:29 +0100

    Updated

--- contrib/monero/tx.sh
+++ contrib/monero/tx.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-python /var/www/underground.pm/run.py -c "$1"
+/var/www/underground.pm/venv/bin/python /var/www/underground.pm/run.py -c "$1"
 logger "Transaction with txid $1 has been recived"

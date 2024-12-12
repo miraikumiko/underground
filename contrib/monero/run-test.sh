16c3ad2b4dc6041d066adb23734b3ee80e26d5f1
underground <underground@underground.pm>
2024-12-12 14:49:20 +0100

    Updated

--- contrib/monero/run-test.sh
+++ contrib/monero/run-test.sh
@@ -1,1 +1,3 @@
+#!/bin/sh
+
 monero-wallet-rpc --testnet --wallet-file "$MONERO_TEST_WALLET_PATH" --password "$MONERO_TEST_WALLET_PASSWORD" --daemon-address "$MONERO_TEST_DAEMON_ADDRESS" --trusted-daemon --rpc-bind-port "$MONERO_TEST_RPC_PORT" --rpc-login "$MONERO_TEST_RPC_LOGIN" --log-file "$MONERO_TEST_RPC_LOG_PATH" --tx-notify "$MONERO_TEST_TX_PATH %s"

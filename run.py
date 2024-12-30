ebee5836791a2b7312821cc33c07703c0a2f2d6b
underground <underground@underground.pm>
2024-12-30 14:38:25 +0100

    Updated

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
+import asyncio
 import uvicorn
 from src.main import app
 from src.config import HOST, PORT
 
-if __name__ == "__main__":
+
+async def main():
     uvicorn.run("run:app", host=HOST, port=PORT, reload=True)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

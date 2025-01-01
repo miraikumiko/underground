35407344a446b49f4afcb3d0bf1aa775014199c9
underground <underground@underground.pm>
2025-01-02 00:28:02 +0100

    Updated

--- src/config.py
+++ src/config.py
@@ -6,22 +6,22 @@ BASE_PATH = path.dirname(path.dirname(path.realpath(__file__)))
 config = Config(f"{BASE_PATH}/.env")
 
 HOST = config("HOST")
-PORT = int(config("PORT"))
+PORT = config("PORT", cast=int)
 
 DB_PATH = config("DB_PATH")
 IMAGES_PATH = config("IMAGES_PATH")
 
-REGISTRATION = int(config("REGISTRATION"))
-TOKEN_EXPIRY_DAYS = int(config("TOKEN_EXPIRY_DAYS"))
+REGISTRATION = config("REGISTRATION", cast=bool)
+TOKEN_EXPIRY_DAYS = config("TOKEN_EXPIRY_DAYS", cast=int)
 
-VDS_DAYS = int(config("VDS_DAYS"))
-VDS_MAX_PAYED_DAYS = int(config("VDS_MAX_PAYED_DAYS"))
-VDS_EXPIRED_DAYS = int(config("VDS_EXPIRED_DAYS"))
+VDS_DAYS = config("VDS_DAYS", cast=int)
+VDS_MAX_PAYED_DAYS = config("VDS_MAX_PAYED_DAYS", cast=int)
+VDS_EXPIRED_DAYS = config("VDS_EXPIRED_DAYS", cast=int)
 
 REDIS_HOST = config("REDIS_HOST")
-REDIS_PORT = int(config("REDIS_PORT"))
+REDIS_PORT = config("REDIS_PORT", cast=int)
 
 MONERO_RPC_IP = config("MONERO_RPC_IP")
-MONERO_RPC_PORT = int(config("MONERO_RPC_PORT"))
+MONERO_RPC_PORT = config("MONERO_RPC_PORT", cast=int)
 MONERO_RPC_USER = config("MONERO_RPC_USER")
 MONERO_RPC_PASSWORD = config("MONERO_RPC_PASSWORD")
